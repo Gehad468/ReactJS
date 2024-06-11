f3e014eebfdd40b0e6f27af6093cb45cd2c5c52a
Yasmeen Mohamed <135316122+YasmeenMohamed1@users.noreply.github.com>
2024-06-11 12:59:57 +0300

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,9 @@
 import React from 'react';
-
+import Header from '../components/header';
 const AboutUs = () => {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <h1 className="text-center mb-4">من نحن؟</h1>
-        </div>
-      </div>
-    
+    <div className="container my-5">
+      <Header content="من نحن ؟"/>
       <div className="row">
         <div className="col-12">
           <div className="content p-5" style={{backgroundColor: '#FAFAFA' }}>
