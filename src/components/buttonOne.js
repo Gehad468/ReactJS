a4041aa7cf625d0154c355a56c6e81ade3b69603
Yasmeen Mohamed <135316122+YasmeenMohamed1@users.noreply.github.com>
2024-06-11 16:57:47 +0300

    Update buttonOne.js

--- src/components/buttonOne.js
+++ src/components/buttonOne.js
@@ -1,6 +1,6 @@
 function ButtonOne(props){
 return(<>
-<button className="btn px-5 py-4 rounded-pill mb-4 d-block mx-auto" style={{ background: "linear-gradient(to bottom, #135D66 0%, #22A6B6 100%)", color: "white" }}>
+<button className="btn px-5 py-4 rounded-pill mb-4 d-block mx-auto" style={{ background: "linear-gradient(to bottom, #135D66 0%, #22A6B6 100%)", color: "white", fontSize: "2rem" }}>
   {props.content}
 </button>
 </>)
