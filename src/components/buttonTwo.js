5552f14e4bdd9d4d6cfdcd1dd572f9361f3090fe
Yasmeen Mohamed <135316122+YasmeenMohamed1@users.noreply.github.com>
2024-06-11 16:58:10 +0300

    Update buttonTwo.js

--- src/components/buttonTwo.js
+++ src/components/buttonTwo.js
@@ -1,6 +1,6 @@
 function ButtonTwo(props){
     return(<>
-    <button className="btn px-5 py-4 mb-4 d-block mx-auto" style={{ background: "linear-gradient(to bottom, #135D66 0%, #22A6B6 100%)", color: "white"}}>
+    <button className="btn px-5 py-4 mb-4 d-block mx-auto" style={{ background: "linear-gradient(to bottom, #135D66 0%, #22A6B6 100%)", color: "white", fontSize: "2rem" }}>
       {props.content}
     </button>
     </>)
