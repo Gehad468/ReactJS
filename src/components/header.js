618856f641a5d699e45e5f99ea3dec31b812beea
Yasmeen Mohamed <135316122+YasmeenMohamed1@users.noreply.github.com>
2024-06-11 13:01:03 +0300

    Update header.js

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+function Header(props){
+    return(<>
+    <div className="row">
+    <div className="col-12 mb-4">
+        <h1 className="text-center mb-4"> {props.content}</h1>
+      </div>
+      </div>
+    </>)
+    
+    
+    }
+    export default Header;
