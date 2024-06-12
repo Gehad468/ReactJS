143e3dfedf184dfecd54eed0aea0a22adc0038af
Yasmeen Mohamed <135316122+YasmeenMohamed1@users.noreply.github.com>
2024-06-12 09:53:53 +0300

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,18 @@
-function Header(props){
-    return(<>
-    <div className="row">
-    <div className="col-12 mb-4">
-        <h1 className="text-center mb-4"> {props.content}</h1>
-      </div>
-      </div>
-    </>)
-    
-    
-    }
-    export default Header;
+import React from 'react';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function Header(props) {
+    return (
+        <>
+            <div className="row">
+                <div className="col-12 mb-4 d-flex align-items-center justify-content-center">
+                    <FontAwesomeIcon icon={faArrowRight} style={{ color: "#135D66", fontSize: "25px", marginRight: "5px" }} />
+                    <h1 className="mb-0 text-center" style={{ color: "#135D66", fontSize: "clamp(1.5rem, 4vw, 2.5rem)", flex: "1" }}>{props.content}</h1>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Header;
