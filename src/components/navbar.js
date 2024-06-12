e2d5c21f41b613289fec3b7dbff01ce176be73d8
Yasmeen Mohamed <135316122+YasmeenMohamed1@users.noreply.github.com>
2024-06-12 11:14:53 +0300

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,22 +1,64 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import logo from '../assets/logo.png';
+import { NavLink } from "react-router-dom";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from "../assets/logo.png";
 
 const CustomNavbar = () => {
   return (
-    <Navbar expand="lg" bg="light" variant="light" style={{ background: "linear-gradient(to bottom, #135D66 0%, #22A6B6 100%)" }}>
+    <Navbar
+      expand="lg"
+      bg="light"
+      variant="light"
+      style={{
+        background: "linear-gradient(to bottom, #135D66 0%, #22A6B6 100%)",
+      }}
+    >
       <Container>
         <Navbar.Brand href="/">
-          <img src={logo} alt="Logo" style={{ width: "150px", height: "100px", marginLeft: "100px" }} />
+          <img
+            src={logo}
+            alt="Logo"
+            style={{ width: "150px", height: "100px", marginLeft: "100px" }}
+          />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto p-3" style={{ justifyContent: "space-between", width: "100%" }}>
-            <Nav.Link as={NavLink} to="/" activeClassName="active" exact style={{ color: "white" }}>الرئيسية</Nav.Link>
-            <Nav.Link as={NavLink} to="/about" activeClassName="active" style={{ color: "white" }}>من نحن</Nav.Link>
-            <Nav.Link as={NavLink} to="/contact" activeClassName="active" style={{ color: "white" }}>تواصل معنا</Nav.Link>
-            <Nav.Link as={NavLink} to="/signup" activeClassName="active" style={{ color: "white" }}>إنشاء حساب</Nav.Link>
+          <Nav
+            className="ms-auto p-3"
+            style={{ justifyContent: "space-between", width: "100%" }}
+          >
+            <Nav.Link
+              as={NavLink}
+              to="/"
+              activeclassname="active"
+              style={{ color: "white" }}
+            >
+              الرئيسية
+            </Nav.Link>
+            <Nav.Link
+              as={NavLink}
+              to="/about"
+              activeclassname="active"
+              style={{ color: "white" }}
+            >
+              من نحن
+            </Nav.Link>
+            <Nav.Link
+              as={NavLink}
+              to="/contact"
+              activeclassname="active"
+              style={{ color: "white" }}
+            >
+              تواصل معنا
+            </Nav.Link>
+            <Nav.Link
+              as={NavLink}
+              to="/signup"
+              activeclassname="active"
+              style={{ color: "white" }}
+            >
+              إنشاء حساب
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
