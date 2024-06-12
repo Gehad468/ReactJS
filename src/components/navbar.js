d04535a33dad325472ece1f6257fca7ccbdcd312
Yasmeen Mohamed <135316122+YasmeenMohamed1@users.noreply.github.com>
2024-06-12 09:28:38 +0300

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,39 +1,27 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import logo from '../assets/logo.png';
-const Navbar = () => {
+
+const CustomNavbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light pb-2" style={{ background: "linear-gradient(to bottom, #135D66 0%, #22A6B6 100%)" }}>
-      <div className="container">
-        <div className="d-flex justify-content-between align-items-center w-100">
-          <div className="d-flex align-items-center">
-            <NavLink className="navbar-brand ml-5" to="/">
-            <img src={logo} alt="Logo" style={{ width: "150px", height: "100px" }} />
-            </NavLink>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-          </div>
-          <div className="collapse navbar-collapse mx-5" id="navbarSupportedContent">
-            <ul className="navbar-nav justify-content-between align-items-center w-100">
-              <li className="nav-item">
-                <NavLink className="nav-link text-white" activeClassName="active" exact to="/">الرئيسية</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link text-white" activeClassName="active" to="/about">من نحن</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link text-white" activeClassName="active" to="/contact">تواصل معنا</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link text-white" activeClassName="active" to="/signup">إنشاء حساب</NavLink>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <Navbar expand="lg" bg="light" variant="light" style={{ background: "linear-gradient(to bottom, #135D66 0%, #22A6B6 100%)" }}>
+      <Container>
+        <Navbar.Brand href="/">
+          <img src={logo} alt="Logo" style={{ width: "150px", height: "100px", marginLeft: "100px" }} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto p-3" style={{ justifyContent: "space-between", width: "100%" }}>
+            <Nav.Link as={NavLink} to="/" activeClassName="active" exact style={{ color: "white" }}>الرئيسية</Nav.Link>
+            <Nav.Link as={NavLink} to="/about" activeClassName="active" style={{ color: "white" }}>من نحن</Nav.Link>
+            <Nav.Link as={NavLink} to="/contact" activeClassName="active" style={{ color: "white" }}>تواصل معنا</Nav.Link>
+            <Nav.Link as={NavLink} to="/signup" activeClassName="active" style={{ color: "white" }}>إنشاء حساب</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default CustomNavbar;
