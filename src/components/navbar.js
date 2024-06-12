f4de1c92e2fd297ce32f963d2801416b7d2ac68b
Yasmeen Mohamed <135316122+YasmeenMohamed1@users.noreply.github.com>
2024-06-12 08:40:31 +0300

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,15 @@
-import React, { useState } from "react";
-import { Link } from 'react-router-dom';
+import React from "react";
+import { NavLink } from 'react-router-dom';
 import logo from '../assets/logo.png';
 const Navbar = () => {
-  const [activeLink, setActiveLink] = useState(null);
-
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light pb-2" style={{ background: "linear-gradient(to bottom, #135D66 0%, #22A6B6 100%)" }}>
       <div className="container">
         <div className="d-flex justify-content-between align-items-center w-100">
           <div className="d-flex align-items-center">
-            <Link className="navbar-brand ml-5" to="/">
-              <img src={logo} alt="Logo" style={{ width: "180px", height: "130px" }} />
-            </Link>
+            <NavLink className="navbar-brand ml-5" to="/">
+            <img src={logo} alt="Logo" style={{ width: "150px", height: "100px" }} />
+            </NavLink>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
@@ -19,16 +17,16 @@ const Navbar = () => {
           <div className="collapse navbar-collapse mx-5" id="navbarSupportedContent">
             <ul className="navbar-nav justify-content-between align-items-center w-100">
               <li className="nav-item">
-                <Link className="nav-link text-white" activeClassName="active" exact to="/">الرئيسية</Link>
+                <NavLink className="nav-link text-white" activeClassName="active" exact to="/">الرئيسية</NavLink>
               </li>
               <li className="nav-item">
-                <Link className={`nav-link text-white ${activeLink === 'about' ? 'active' : ''}`} to="/about" onMouseOver={() => setActiveLink('about')} onMouseLeave={() => setActiveLink(null)}>من نحن</Link>
+                <NavLink className="nav-link text-white" activeClassName="active" to="/about">من نحن</NavLink>
               </li>
               <li className="nav-item">
-                <Link className="nav-link text-white" activeClassName="active" to="/contact">تواصل معنا</Link>
+                <NavLink className="nav-link text-white" activeClassName="active" to="/contact">تواصل معنا</NavLink>
               </li>
               <li className="nav-item">
-                <Link className="nav-link text-white" activeClassName="active" to="/signup">إنشاء حساب</Link>
+                <NavLink className="nav-link text-white" activeClassName="active" to="/signup">إنشاء حساب</NavLink>
               </li>
             </ul>
           </div>
