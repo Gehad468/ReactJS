7ed9037a514cbd2f053d0e1ef502edeb8d1dfea0
Yasmeen Mohamed <135316122+YasmeenMohamed1@users.noreply.github.com>
2024-06-28 13:11:14 +0300

    Update buttonOne.js

--- src/components/buttonOne.js
+++ src/components/buttonOne.js
@@ -1,10 +1,10 @@
-function ButtonOne(props) {
+function ButtonOne({ content, onClick }) {
   return (<span className="m-4">
 
     <button className="btn px-5 py-2 rounded-pill mb-4  mx-auto " style={{
       background: "linear-gradient(to bottom, #135D66 0%, #22A6B6 100%)", color: "white" ,fontSize:"1.2rem"
-    }}>
-      {props.content}
+    }} onClick={onClick}>
+      {content}
     </button>
   </span>)
 
