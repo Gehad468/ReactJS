bb09a36ea7db3e7699af3d0f9d527ad40b320894
Yasmeen Mohamed <135316122+YasmeenMohamed1@users.noreply.github.com>
2024-08-31 21:00:33 +0300

    Update buttonOne.js

--- src/components/buttonOne.js
+++ src/components/buttonOne.js
@@ -1,14 +1,30 @@
-function ButtonOne({ content, onClick }) {
-  return (<span className="m-4">
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-    <button className="btn px-5 py-2 rounded-pill mb-4  mx-auto " style={{
-      background: "linear-gradient(to bottom, #135D66 0%, #22A6B6 100%)", color: "white" ,fontSize:"1.2rem"
-    }} onClick={onClick}>
-      {content}
-    </button>
-  </span>)
+function ButtonOne({ content, href }) {
+  const navigate = useNavigate();
 
+  const handleClick = () => {
+    if (href) {
+      navigate(href);
+    }
+  };
 
+  return (
+    <span className="m-4">
+      <button
+        className="btn px-5 py-2 rounded-pill mb-4 mx-auto"
+        style={{
+          background: "linear-gradient(to bottom, #135D66 0%, #22A6B6 100%)",
+          color: "white",
+          fontSize: "1.2rem",
+        }}
+        onClick={handleClick}
+      >
+        {content}
+      </button>
+    </span>
+  );
 }
 
 export default ButtonOne;
