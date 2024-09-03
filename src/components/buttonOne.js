8f51ff44a0d8049c6487c20829963397f0785671
Yasmeen Mohamed <135316122+YasmeenMohamed1@users.noreply.github.com>
2024-09-03 19:55:13 +0300

    Update buttonOne.js

--- src/components/buttonOne.js
+++ src/components/buttonOne.js
@@ -1,11 +1,43 @@
+// import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+
+// function ButtonOne({ content, href }) {
+//   const navigate = useNavigate();
+
+//   const handleClick = () => {
+//     if (href) {
+//       navigate(href);
+//     }
+//   };
+
+//   return (
+//     <span className="m-4">
+//       <button
+//         className="btn px-5 py-2 rounded-pill mb-4 mx-auto"
+//         style={{
+//           background: "linear-gradient(to bottom, #135D66 0%, #22A6B6 100%)",
+//           color: "white",
+//           fontSize: "1.2rem",
+//         }}
+//         onClick={handleClick}
+//       >
+//         {content}
+//       </button>
+//     </span>
+//   );
+// }
+
+// export default ButtonOne;
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-function ButtonOne({ content, href }) {
+function ButtonOne({ content, href, onClick }) {
   const navigate = useNavigate();
 
-  const handleClick = () => {
-    if (href) {
+  const handleClick = (event) => {  
+    if (onClick) {  
+      onClick(event);  
+    } else if (href) { 
       navigate(href);
     }
   };
@@ -19,7 +51,7 @@ function ButtonOne({ content, href }) {
           color: "white",
           fontSize: "1.2rem",
         }}
-        onClick={handleClick}
+        onClick={handleClick} 
       >
         {content}
       </button>
